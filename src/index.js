555f42a3e1eb3edc139d3f523c4ab33265258418
andresrgallo <andresgallo@hotmail.com>
2019-02-28 14:27:44 +1100

    Add style and color function

--- src/index.js
+++ src/index.js
@@ -2,15 +2,58 @@ import 'index.scss';
 
 var theQuotes;
 
-// function createNode(element) {
-// 	return document.createElement(element);
-// }
+var colors = [
+	'#16a085',
+	'#27ae60',
+	'#2c3e50',
+	'#f39c12',
+	'#e74c3c',
+	'#9b59b6',
+	'#FB6964',
+	'#342224',
+	'#472E32',
+	'#BDBB99',
+	'#77B1A9',
+	'#73A857'
+];
 
-// function append(parent, el) {
-// 	return parent.appendChild(el);
-// }
+//Variable declarations
+var currentQuote =
+	'A good teacher can inspire hope, ignite the imagination, and instill a love of learning';
+var currentAuthor = 'Brad Henry';
+var rootApp = document.getElementById('root');
+var text = document.getElementById('text');
+var author = document.getElementById('author');
+var quotesIcon = document.getElementById('quote-text');
+var twitter = document.getElementById('tweet-icon');
+var theButton = document.getElementById('new-quote');
 
-const strong = document.getElementById('text');
+//Initial text to render
+text.innerHTML = currentQuote;
+author.innerHTML = currentAuthor;
+
+//Select a random color from the color array
+function randomColor() {
+	return colors[Math.floor(Math.random() * colors.length)];
+}
+
+//Assign random color to Body, button, text and tweet icon
+rootApp.style.backgroundColor = randomColor();
+var theColor = document.getElementById('root').style.backgroundColor;
+
+function handleColor(someColor) {
+	text.style.color = someColor;
+	author.style.color = someColor;
+	twitter.style.color = someColor;
+	quotesIcon.style.color = someColor;
+	theButton.style.backgroundColor = someColor;
+}
+handleColor(theColor);
+
+const tweetQuote = document.getElementById('tweet-quote');
+tweetQuote.href =
+	'https://twitter.com/intent/tweet?hashtags=quotes&text=' +
+	encodeURIComponent('"' + currentQuote + '" ' + currentAuthor);
 
 fetch(
 	'https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json'
@@ -33,9 +76,26 @@ function initialize() {
 	button.addEventListener('click', displayQuote);
 
 	function displayQuote() {
-		let randomQuote = theQuotes[Math.floor(Math.random() * 100)];
-		document.getElementById('text').innerHTML = randomQuote.quote;
-		document.getElementById('author').innerHTML = randomQuote.author;
+		var randomQuote = theQuotes[Math.floor(Math.random() * theQuotes.length)];
+		currentQuote = randomQuote.quote;
+		currentAuthor = randomQuote.author;
+		$('#quote-text').animate({ opacity: 0 }, 500, function() {
+			$(this).animate({ opacity: 1 }, 500);
+			$('#text').text(randomQuote.quote);
+		});
+
+		$('#quote-author').animate({ opacity: 0 }, 500, function() {
+			$(this).animate({ opacity: 1 }, 500);
+			$('#author').text(randomQuote.author);
+		});
+		var newColor = randomColor();
+		rootApp.style.backgroundColor = newColor;
+		handleColor(newColor);
+		document.getElementsByTagName('svg')[1].style.color =
+			rootApp.style.backgroundColor;
+		tweetQuote.href =
+			'https://twitter.com/intent/tweet?hashtags=quotes&text=' +
+			encodeURIComponent('"' + currentQuote + '" ' + currentAuthor);
 	}
 }
 
